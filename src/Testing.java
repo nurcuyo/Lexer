cdcd8b65e2aa2dc648597dcfe08727cda746a502
Niko <nikolas.urcuyo@gmail.com>
2021-02-07 13:22:41 -0500

    Testing Commits

---
+++ src/Testing.java
@@ -0,0 +1,8 @@
+public class Testing {
+    static int add (int x, int y){
+        return x + y;
+    }
+    public static void main(String[] args) {
+        System.out.println(add(5,6));
+    }
+}
