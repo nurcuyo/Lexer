a2a3ae2fccfab0cadc4c61ba20c15b87c6ec05b6
Niko <nikolas.urcuyo@gmail.com>
2021-02-07 14:26:24 -0500

    All Files set up

--- src/Lexer.java
+++ src/Lexer.java
@@ -3,5 +3,83 @@ import java.util.List;
 public final class Lexer {
 
     private final CharStream chars;
-    //this comment is a change
+
+    public Lexer(String input) {
+        chars = new CharStream(input);
+    }
+
+    public List<Token> lex() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexToken() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexIdentifier() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexNumber() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexCharacter() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexString() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexEscape() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexOperator() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public boolean peek() {
+        return false; //LECTURE LECTURE LECTURE TODO
+    }
+
+    public boolean match() {
+        return false; //LECTURE LECTURE LECTURE TODO
+    }
+
+    public static final class CharStream {
+
+        private final String input;
+        private int index = 0;
+        private int length = 0;
+
+        public CharStream(String input) {
+            this.input = input;
+        }
+
+        public boolean has(int offset) {
+            return index + offset < input.length();
+        }
+
+        public char get(int offset){
+            return input.charAt(index + offset);
+        }
+
+        public void advance() {
+            index ++;
+            length ++;
+        }
+
+        public void skip() {
+            length = 0;
+        }
+
+        public Token emit(Token.Type type) {
+            int start = index - length;
+            skip();
+            return new Token(type, input.substring(start, index), start);
+        }
+    }
 }
+
---
+++ src/ParseException.java
@@ -0,0 +1,14 @@
+public final class ParseException extends RuntimeException {
+
+    private final int index;
+
+    public ParseException(String message, int index) {
+        super(message);
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+}
